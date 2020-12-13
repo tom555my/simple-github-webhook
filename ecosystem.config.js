badbbf5b0fd6414fed9333a48b8693c58932025f
Tommy Chung <tommy.chung@datatech.ai>
2020-12-13 21:23:54 +0800

    Remove server.js, Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'simple-github-webhook',
-      script: 'server.js',
+      script: 'yarn start',
     },
   ],
 };
--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const micro = require('micro');
-const index = require('./index');
-
-const server = new http.Server(micro(index));
-
-server.listen(5000);
