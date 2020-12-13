f940d3e7e22b1f0605534cadb83becc796c9193c
Tommy Chung <tommy.chung@datatech.ai>
2020-12-13 21:38:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ module.exports = async (req, res) => {
   const repos = Object.keys(config);
   const body = await json(req);
   const repo = body.repository.name;
+  console.log('Repo', repo);
   if (!repos.includes(repo)) {
     send(res, 404, 'Not Found');
     return;
@@ -36,6 +37,7 @@ module.exports = async (req, res) => {
     return;
   }
   const branch = body.ref.split('/')[body.ref.split('/').length - 1];
+  console.log('Branch', branch);
   if (!Object.keys(config[repo]).includes(branch)) {
     send(res, 400, {
       error: `No CMD for branch ${branch}`,
@@ -46,6 +48,7 @@ module.exports = async (req, res) => {
   for (let index = 0; index < commands.length; index++) {
     const command = commands[index];
     try {
+      console.log(`Step ${index + 1}: ${command}`);
       const output = await execAsync(command);
       send(res, 200, { message: output });
     } catch (error) {
