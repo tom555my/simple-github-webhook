272ca8e8b3ee05c535f4859de6d59691a4cc1a26
Tommy Chung <tommy.chung@datatech.ai>
2020-12-13 14:03:39 +0800

    Init

---
+++ README.md
@@ -0,0 +1,52 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Simple Github Webhook
+
+This project is aimed at to create simple github webhook server for quick and dirty CI/CD integration.
+
+## Tech stack
+
+* NodeJs
+* [Vercel/Micro](https://github.com/vercel/micro)
+
+## Usage
+
+1. Edit config json
+
+```json
+{
+  [branch name]: {
+    "cmd": [
+      // List of string of command
+    ]
+  }
+}
+
+// For example
+{
+  "main": {
+    "cmd": [
+      "cd /home/tom555my/repo/simple-github-webhook",
+      "git checkout main",
+      "git pull origin main",
+      "yarn",
+      "yarn build",
+      "yarn start"
+    ]
+  }
+}
+```
+
+2. Start the server
+
+`yarn start`
+
+3. Config your github repo webhook
+
+https://github.com/\<username>/\<your-repo>/settings/hooks
+
+# License
+
+MIT
+
+Copyright (c) 2020-present, Chung Kwok Cheong
---
+++ index.js
@@ -0,0 +1,50 @@
+const { json, send } = require("micro");
+const config = require("./config.json");
+const exec = require("child_process").exec;
+
+const execAsync = (command) =>
+  new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error.message);
+        return;
+      }
+      if (stderr) {
+        reject(stderr);
+        return;
+      }
+      resolve(stdout);
+    });
+  });
+
+module.exports = async (req, res) => {
+  const body = await json(req);
+  if (body.ref == null) {
+    send(res, 400, {
+      error: "missing-ref",
+    });
+    return;
+  }
+  const branch = body.ref.split("/")[body.ref.split("/").length - 1];
+  if (!Object.keys(config).includes(branch)) {
+    send(res, 400, {
+      error: `No CMD for branch ${branch}`,
+    });
+    return;
+  }
+  for (let index = 0; index < config[branch].cmd.length; index++) {
+    const command = config[branch].cmd[index];
+    try {
+      const output = await execAsync(command);
+      send(res, 200, { message: output });
+    } catch (error) {
+      send(res, 400, {
+        error,
+      });
+      return;
+    }
+  }
+  res.end("Done exec");
+};
+
+// cd ~/chatbot-server && bash deploy.sh
